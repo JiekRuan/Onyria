c5e24066505fa5d54055d66b825e7a515cb640f9
JiekRuan <ruanjiek2001@gmail.com>
2025-06-24 15:46:15 +0200

    update architecture

--- DreamProject/Onyria/asgi.py
+++ DreamProject/Onyria/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for diary project.
+ASGI config for Onyria project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'diary.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Onyria.settings')
 
 application = get_asgi_application()
