a585293daa8a6bb5530a3f1e9b587cce7716460a
Adrien Formoso <adrien.formoso@live.fr>
2025-06-25 10:34:59 +0200

    login / inscription / deconec

--- DreamProject/accounts/views.py
+++ DreamProject/accounts/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from .forms import RegisterForm, LoginForm
 
-# Create your views here.
+def register_view(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return render(request, 'home.html', {'user': request.user})  # 'home' doit correspondre au nom dans urls.py
+    else:
+        form = RegisterForm()
+    return render(request, 'register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user = authenticate(request, email=email, password=password)
+            if user:
+                login(request, user)
+                return render(request, 'home.html', {'user': request.user})
+    else:
+        form = LoginForm()
+    return render(request, 'login.html', {'form': form})
+
+def home_view(request):
+    return render(request, 'home.html', {'user': request.user})
+
+def logout_view(request):
+    logout(request)
+    return login_view(request)
