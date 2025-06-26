5e9c64539d1256d9cdeb58b77a8b453288ace503
tanyatibouche <tanyatib12@gmail.com>
2025-06-26 19:03:03 +0200

    init commit

--- DreamProject/accounts/tests.py
+++ DreamProject/accounts/tests.py
@@ -1,3 +1,33 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AnonymousUser
+from django.contrib.messages import get_messages
+from .forms import RegisterForm, LoginForm
+from .models import CustomUser
 
-# Create your tests here.
+User = get_user_model()
+
+
+class CustomUserModelTests(TestCase):
+    """Tester le custom user"""
+
+    def set_up(self):
+        self.user_data = {
+            'email': 'test@gmail.com',
+            'username': 'test_user',
+            'password': 'Test_password123!',
+        }
+
+    def test_create_user(self):
+        """Tester la création d'un user normal"""
+        user = User.objects.create_user(
+            email=self.user_data['email'],
+            username=self.user_data['username'],
+            password=self.user_data['password'],
+        )
+
+        self.assertEqual(user.email, self.user_data['email'])
+        self.assertEqual(user.username, self.user_data['username'])
+        self.assertTrue(user.check_password(self.user_data['password']))
+        self.assertTrue(user.is_active)
