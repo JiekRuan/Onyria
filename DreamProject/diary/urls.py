43e06dac7b9c0a7f953ab14482a8992f6dbe1101
Emma Coco <emma.coco.pro@gmail.com>
2025-08-04 16:59:30 +0200

    Boutons de navigation dans l'app diary

--- DreamProject/diary/urls.py
+++ DreamProject/diary/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.dream_diary_view, name='dream_diary'),
-    path('record/', views.dream_recorder_view, name='dream-recorder'),
+    path('record/', views.dream_recorder_view, name='dream_recorder'),
     path('transcribe/', views.transcribe, name='transcribe'),
     path('analyse_from_voice/', views.analyse_from_voice, name='analyse_from_voice'),
     path('followup/', views.diary_followup, name='diary_followup'),
