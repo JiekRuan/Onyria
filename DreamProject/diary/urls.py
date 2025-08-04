1d11b38a407da11496cae86c5e4bd879b2d12079
Emma Coco <emma.coco.pro@gmail.com>
2025-08-04 14:50:15 +0200

    Création d'un placeholder pour le suivi des rêves : dream_followup

--- DreamProject/diary/urls.py
+++ DreamProject/diary/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('record/', views.dream_recorder_view, name='dream-recorder'),
     path('transcribe/', views.transcribe, name='transcribe'),
     path('analyse_from_voice/', views.analyse_from_voice, name='analyse_from_voice'),
+    path('followup/', views.diary_followup, name='diary_followup'),
 ]
