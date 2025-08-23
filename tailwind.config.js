ba08811adc2c3c168a3fbca8d1b3975d5498f4ad
Emma Coco <emma.coco.pro@gmail.com>
2025-08-23 21:58:32 +0200

    Fix : font appliquée au body

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
-    "./DreamProject/**/*.html",        // Tous tes templates Django dans le projet principal
-    "./diary/templates/**/*.html",     // Templates dans l'app diary
-    "./accounts/templates/**/*.html",  // Templates dans l'app accounts
+    "./DreamProject/**/*.html",
+    "./diary/templates/**/*.html",
+    "./accounts/templates/**/*.html",
+    "./frontend/**/*.js",
+    "./js/**/*.js",
   ],
   theme: {
     extend: {
+      // 1) On remplace font-sans par Schibsted Grotesk (par défaut partout)
       fontFamily: {
+        sans: ['"Schibsted Grotesk"', ...defaultTheme.fontFamily.sans],
+        // 2) (optionnel) on garde une utilitaire dédiée au cas où
         grotesk: ['"Schibsted Grotesk"', 'sans-serif'],
       },
     },
